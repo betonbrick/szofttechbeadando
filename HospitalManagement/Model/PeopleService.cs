53514bbd3927dbaf9f28e5b7562fb6eca8b64e02
JU574U53R <illes.varga25@gmail.com>
2021-11-20 12:36:25 +0100

    PeopleService osztály - összes alkalmazott és beteg függvényének megírása.

--- HospitalManagement/Model/PeopleService.cs
+++ HospitalManagement/Model/PeopleService.cs
@@ -6,7 +6,74 @@ using System.Threading.Tasks;
 
 namespace HospitalManagement.Model
 {
-    class PeopleService
+    public class PeopleService
     {
+
+        private hospitalmgmntEntities employeeEntities;
+        private hospitalmgmntEntities patientEntities;
+
+        public PeopleService()
+        {
+            employeeEntities = new hospitalmgmntEntities();
+            patientEntities = new hospitalmgmntEntities();
+        }
+
+        // EMPLOYEE TÁBLA ÖSSZES ELEME
+        public List<EmployeeDTO> getAllEmployees()
+        {
+            List<EmployeeDTO> employeeDTOs = new List<EmployeeDTO>();
+            try
+            {
+                var employees = from employee in employeeEntities.Employees select employee;
+                foreach (var employee in employees)
+                {
+                    employeeDTOs.Add(new EmployeeDTO
+                    {
+                        Id = employee.Id,
+                        Name = employee.Name,
+                        Age = employee.Age,
+                        Settlement = employee.Settlement,
+                        Address = employee.Address,
+                        Email = employee.Email,
+                        Phone = employee.Phone,
+                        Speciality = employee.Speciality,
+                        Salary = employee.Salary
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return employeeDTOs;
+        }
+
+        // PATIENT TÁBLA ÖSSZES ELEME
+        public List<PatientDTO> getAllPatients()
+        {
+            List<PatientDTO> patientDTOs = new List<PatientDTO>();
+            try
+            {
+                var patients = from patient in patientEntities.Patients select patient;
+                foreach (var patient in patients)
+                {
+                    patientDTOs.Add(new PatientDTO
+                    {
+                        Id = patient.Id,
+                        Name = patient.Name,
+                        Age = patient.Age,
+                        Settlement = patient.Settlement,
+                        Address = patient.Address,
+                        Email = patient.Email,
+                        Phone = patient.Phone
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return patientDTOs;
+        }
     }
 }
