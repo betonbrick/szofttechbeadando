79b784d62f2414f0b9641e6bc32250fd47005fbf
JU574U53R <illes.varga25@gmail.com>
2021-11-20 12:19:15 +0100

    Adatbázis és model implementálásához szükséges osztályok létrehozása.

---
+++ HospitalManagement/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace HospitalManagement.ViewModel.Base
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
