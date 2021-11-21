8a648f5ce98d6c7ffa196a3214506dd0d6fbad56
Miklós Barabás <barabasmiklos@outlook.com>
2021-11-21 12:47:05 +0100

    datacontext EmployeesView

--- HospitalManagement/View/EmployeesView.xaml.cs
+++ HospitalManagement/View/EmployeesView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HospitalManagement.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace HospitalManagement.View
     /// </summary>
     public partial class EmployeesView : UserControl
     {
+        EmployeeViewModel employeeViewModel;
         public EmployeesView()
         {
             InitializeComponent();
+            employeeViewModel = new EmployeeViewModel();
+            DataContext = employeeViewModel;
+            
         }
     }
 }
