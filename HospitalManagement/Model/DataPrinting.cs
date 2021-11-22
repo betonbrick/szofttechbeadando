c025d1bc9f7c659b3838d443cea5b159aa455f47
Miklós Barabás <barabasmiklos@outlook.com>
2021-11-22 21:39:08 +0100

    már megjelenik a nyomtatáskor. A szélességet fixálni kell még

--- HospitalManagement/Model/DataPrinting.cs
+++ HospitalManagement/Model/DataPrinting.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
+
 using HospitalManagement.View;
 
 namespace HospitalManagement.ViewModel
