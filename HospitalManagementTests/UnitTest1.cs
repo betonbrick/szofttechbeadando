de6b3cf906b3d246c79717385efbb33548dfbb87
Miklós Barabás <barabasmiklos@outlook.com>
2021-12-19 22:18:32 +0100

    Unit teszteléshez szükséges Nuget csomagok telepítése, unit teszt írása

---
+++ HospitalManagementTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using HospitalManagement.Model;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace HospitalManagementTests
+{
+    public class UnitTest1
+    {
+        Hashing hash = new Hashing();
+        [Fact]
+        public void Create_MD5Hash_Should_Work()
+        {
+            string actual = "21232F297A57A5A743894A0E4A801FC3";
+            string expected = hash.createMD5Hash("admin");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
